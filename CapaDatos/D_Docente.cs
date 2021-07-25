b555010cc17dfdf9a24d119880e294b1669f66bb
joel <TeslaTeslon@fastweb.com>
2021-07-25 03:21:15 -0500

    tes1

--- CapaDatos/D_Docente.cs
+++ CapaDatos/D_Docente.cs
@@ -14,5 +14,85 @@ namespace CapaDatos
     class D_Docente
     {
         SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+        public DataTable ListDocente()
+        {
+            DataTable table = new DataTable();
+            SqlDataReader readRows;
+            SqlCommand cmd = new SqlCommand("SP_LISTARDOCENTE", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            readRows = cmd.ExecuteReader();
+            table.Load(readRows);
+
+            readRows.Close();
+            conexion.Close();
+
+            return table;
+        }
+        public DataTable SearchDocente(E_Docente docente)
+        {
+            DataTable tabla = new DataTable();
+            SqlCommand cmd = new SqlCommand("SP_BUSCARDOCENTE", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@BUSCAR", docente.Search);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(tabla);
+
+            conexion.Close();
+            return tabla;
+        }
+
+        public void DeleteDocente(int id)
+        {
+            SqlCommand cmd = new SqlCommand("SP_ELIMINARDOCENTE", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@IDDOCENTE", id);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void CreateDocente(E_Docente docente)
+        {
+
+            SqlCommand cmd = new SqlCommand("SP_INSERTARDOCENTE", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@NOMBRES", docente.Nombres);
+            cmd.Parameters.AddWithValue("@APELLIDOS", docente.Apellidos);
+            cmd.Parameters.AddWithValue("@TITULO", docente.TituloAcademico);
+            cmd.Parameters.AddWithValue("@FACULTAD", docente.Facultad);
+            cmd.Parameters.AddWithValue("@EPROFESIONAL", docente.EscuelaProfesional);
+            cmd.Parameters.AddWithValue("@CATEGORIA", docente.Categoria);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void UpdateDocente(E_Docente docente)
+        {
+            SqlCommand cmd = new SqlCommand("SP_EDITARDOCENTE", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@IDDOCENTE", docente.IdDocente);
+            cmd.Parameters.AddWithValue("@NOMBRES", docente.Nombres);
+            cmd.Parameters.AddWithValue("@APELLIDOS", docente.Apellidos);
+            cmd.Parameters.AddWithValue("@TITULO", docente.TituloAcademico);
+            cmd.Parameters.AddWithValue("@FACULTAD", docente.Facultad);
+            cmd.Parameters.AddWithValue("@EPROFESIONAL", docente.EscuelaProfesional);
+            cmd.Parameters.AddWithValue("@CATEGORIA", docente.Categoria);
+
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
     }
 }
--- CapaEntidades/E_Docente.cs
+++ CapaEntidades/E_Docente.cs
@@ -8,6 +8,23 @@ namespace CapaEntidades
 {
     class E_Docente
     {
+        private int _IdDocente;
+        private string _Nombres;
+        private string _Apellidos;
+        private string _TituloAcademico;
+        private string _Facultad;
+        private string _EscuelaProfesional;
+        private string _Categoria;
 
+        private string search;
+
+        public int IdDocente { get => _IdDocente; set => _IdDocente = value; }
+        public string Nombres { get => _Nombres; set => _Nombres = value; }
+        public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
+        public string TituloAcademico { get => _TituloAcademico; set => _TituloAcademico = value; }
+        public string Facultad { get => _Facultad; set => _Facultad = value; }
+        public string EscuelaProfesional { get => _EscuelaProfesional; set => _EscuelaProfesional = value; }
+        public string Search { get => search; set => search = value; }
+        public string Categoria { get => _Categoria; set => _Categoria = value; }
     }
 }
